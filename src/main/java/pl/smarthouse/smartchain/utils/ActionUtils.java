26c947173ea0b18112824162a0d60e01efdb6afa
majkelp1982 <s.pomazanka@gmail.com>
2023-01-25 20:09:31 +0100

    add predicates

--- src/main/java/pl/smarthouse/smartchain/utils/ActionUtils.java
+++ src/main/java/pl/smarthouse/smartchain/utils/ActionUtils.java
@@ -7,14 +7,21 @@ import pl.smarthouse.smartmodule.model.enums.ActorType;
 
 @UtilityClass
 public class ActionUtils {
-	
-	public void setActionToAllActorType(final ActorMap actorMap, final ActorType actorType, final CommandType commandType) {
+
+	public void setActionToAllActorType(final ActorMap actorMap, final ActorType actorType, final CommandType commandType, final String commandValue) {
 		actorMap.stream()
 				.forEach(
 						actor -> {
 							if (actorType.equals(actor.getType())) {
 								actor.getCommandSet().setCommandType(commandType);
+								actor.getCommandSet().setValue(commandValue);
 							}
 						});
 	}
+
+	public void setActionToAllActorType(final ActorMap actorMap, final ActorType actorType, final CommandType commandType) {
+		setActionToAllActorType(actorMap, actorType, commandType, null);
+	}
+
+
 }
--- src/main/java/pl/smarthouse/smartchain/utils/PredicateUtils.java
+++ src/main/java/pl/smarthouse/smartchain/utils/PredicateUtils.java
@@ -2,8 +2,13 @@ package pl.smarthouse.smartchain.utils;
 
 import lombok.experimental.UtilityClass;
 import pl.smarthouse.smartchain.model.Step;
+import pl.smarthouse.smartmodule.model.actors.actor.Actor;
+import pl.smarthouse.smartmodule.model.actors.actor.ActorMap;
+import pl.smarthouse.smartmodule.model.actors.response.Response;
+import pl.smarthouse.smartmodule.model.enums.ActorType;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 @UtilityClass
@@ -11,5 +16,25 @@ public class PredicateUtils {
 
 	public Predicate<Step> delaySeconds(final int delayInSeconds) {
 		return step -> LocalDateTime.now().isAfter(step.getStartTime().plusSeconds(delayInSeconds));
+	}
+
+	public Predicate<Step> isActorReadCommandSuccessful(final Actor actor) {
+		return step -> {
+			final Response response = actor.getResponse();
+			if (Objects.isNull(response)
+					|| Objects.isNull(response.getResponseUpdate())) {
+				return false;
+			} else {
+				return step.getStartTime().isBefore(response.getResponseUpdate());
+			}
+		};
+	}
+
+	public Predicate<Step> isAllActorTypeReadCommandSuccessful(final ActorMap actorMap, final ActorType actorType) {
+		return step ->
+				actorMap.stream()
+						.filter(actor -> actorType.equals(actor.getType()))
+						.allMatch(actor -> isActorReadCommandSuccessful(actor).test(step));
+
 	}
 }
